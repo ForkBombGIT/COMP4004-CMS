637dc09cb513e771b8df44a6b3a9b302fe351884
Liannus <josh-gorman@hotmail.com>
2020-11-13 17:08:42 -0500

    SETUP add default seeders and migrations for sequelize

---
+++ backend/src/db/schemas/index.js
@@ -0,0 +1,25 @@
+const academicDeadline = require('./academicDeadline.schema');
+const administrator = require('./administrator.schema');
+const application = require('./application.schema');
+const course = require('./course.schema');
+const deliverable = require('./deliverable.schema');
+const enrolled = require('./enrolled.schema');
+const prerequisites = require('./prerequisites.schema');
+const professor = require('./professor.schema');
+const student = require('./student.schema');
+const submits = require('./submits.schema');
+const teaches = require('./teaches.schema');
+
+module.exports = {
+  academicDeadline,
+  administrator,
+  application,
+  course,
+  deliverable,
+  enrolled,
+  prerequisites,
+  professor,
+  student,
+  submits,
+  teaches
+};
