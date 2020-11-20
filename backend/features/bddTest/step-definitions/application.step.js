4d6cee1a9755723a535c94d02475c28d0ca24e94
Liannus <josh-gorman@hotmail.com>
2020-11-20 14:56:42 -0500

    BDD add acceptance testing for student application

---
+++ backend/features/bddTest/step-definitions/application.step.js
@@ -0,0 +1,39 @@
+module.exports = function () {
+  this.Given(/^I am on the login page$/, function (callback) {
+    helpers.loadPage('http://localhost:3000/');
+    callback();
+  });
+
+  this.Then(/^I enter a name in the student name field$/, function (callback) {
+    driver.findElement(By.id('student-name')).sendKeys(
+      shared.studentApplication.studentName
+    );
+    callback();
+  });
+
+  this.When(/^I click the register button$/, function (callback) {
+    driver.findElement(By.id('register-button')).click();
+    driver.sleep(1000).then(() => {
+      callback();
+    });
+  });
+
+  this.Then(/^the system displays an application has been submitted$/, function (callback) {
+    driver.wait(until.elementLocated(by.className('Toastify__toast-body'))).then(() => {
+      const app = shared.app.api.service('application');
+      app.find({
+          query: {
+            $limit: 1,
+            $sort: {
+              createdAt: -1
+            }
+          }
+        }
+      ).then((record) => {
+        console.log(record)
+        app.remove(record[0].id);
+        callback()
+      })
+    });
+  });
+};
