5393cf78213cc2eb3fbcdafb5fe3428573fc3a4f
Liannus <josh-gorman@hotmail.com>
2020-11-20 14:56:16 -0500

    CODE add register form for student application

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,6 +1,6 @@
 const checkObjectContains = (obj1, obj2) => {
-  for (const key of Object.keys(obj1)) {
-    if (!(key in obj2) || obj1[key] !== obj2[key]) {
+  for (const key of Object.keys(obj2)) {
+    if (!(key in obj1) || obj1[key] !== obj2[key]) {
       return false;
     }
   }
