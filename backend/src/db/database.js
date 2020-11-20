2fc16b36737963950a32688dd9f05c6a1a450ded
Liannus <josh-gorman@hotmail.com>
2020-11-19 19:49:33 -0500

    SETUP removing unneeded db stuff

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -5,9 +5,6 @@ const env = process.env.NODE_ENV || 'development';
 
 module.exports = function (app) {
   const config = allConfig[env];
-  // config.database = 'postgres';
-  // config.password = 'postgres';
-  // config.username = 'postgres';
   const sequelize = new Sequelize(config.database, config.username, config.password, config);
   const oldSetup = app.setup;
 
