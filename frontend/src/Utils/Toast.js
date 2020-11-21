a94c9b686369c49ba3ba5dd0636733765193febc
ForkBombGIT <erosdipede@gmail.com>
2020-11-21 12:00:05 -0500

    REFACTOR Replace snackbar component with toast module in login form
    Move like steps from feature specfic definitions to generic file

---
+++ frontend/src/Utils/Toast.js
@@ -0,0 +1,23 @@
+import { toast } from "react-toastify";
+
+export const notifySuccess = (message) =>
+  toast.success(message, {
+    position: "bottom-left",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: false,
+    progress: undefined,
+  });
+
+export const notifyFailure = (message) =>
+  toast.error(message, {
+    position: "bottom-left",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: false,
+    progress: undefined,
+  });
