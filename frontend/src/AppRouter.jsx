47c48bf63db6d7a3a2ed5cd211dd62e3b2713774
ForkBombGIT <erosdipede@gmail.com>
2020-11-21 23:00:09 -0500

    CODE Add auth page redirection

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,20 +1,32 @@
+// import React, { useEffect, useContext, createContext, useState } from "react";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // Redirect,
+  // useHistory,
+  // useLocation,
+} from "react-router-dom";
 import { RouterPaths } from "Utils/";
+import { PrivateRoute, ProvideAuth } from "Utils/RouterAuth";
 import { LoginPage, AdminPage } from "Pages/";
 
 function AppRouter() {
   return (
-    <Router>
-      <Switch>
-        <Route path={RouterPaths.ADMIN}>
-          <AdminPage />
-        </Route>
-        <Route exact path={RouterPaths.LOGIN}>
-          <LoginPage />
-        </Route>
-      </Switch>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <Switch>
+          <Route exact path={RouterPaths.LOGIN}>
+            <LoginPage />
+          </Route>
+          <PrivateRoute path={RouterPaths.ADMIN}>
+            <AdminPage />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </ProvideAuth>
   );
 }
 
---
+++ frontend/src/Utils/RouterAuth.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useContext, createContext, useState } from "react";
+import { Client } from "Server/";
+import { Route, Redirect } from "react-router-dom";
+
+const authContext = createContext();
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+const useProvideAuth = () => {
+  const [user, setUser] = useState(null);
+
+  const signin = (email, password) => {
+    return Client.authenticate({
+      strategy: "local",
+      email,
+      password,
+    })
+      .then((response) => {
+        setUser(response.user);
+        return response.user;
+      })
+      .catch((e) => {
+        console.error(e);
+        return null;
+      });
+  };
+
+  const reAuthenticate = () => {
+    return Client.reAuthenticate()
+      .then((response) => {
+        setUser(response.user);
+        return response.user;
+      })
+      .catch((e) => {
+        console.error(`Couldn't be re-authenticated${e}`);
+        return null;
+      });
+  };
+
+  const signout = () => {
+    return Client.logout().then((response) => {
+      setUser(response.user);
+      return response.user;
+    });
+  };
+
+  useEffect(() => {
+    reAuthenticate();
+  }, []);
+
+  return {
+    user,
+    signin,
+    reAuthenticate,
+    signout,
+  };
+};
+
+export const ProvideAuth = ({ children }) => {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
+
+export const PrivateRoute = ({ children, ...rest }) => {
+  const auth = useAuth();
+  const [authenticating, setAuthenticating] = useState(true);
+  useEffect(() => {
+    auth
+      .reAuthenticate()
+      .then(() => {
+        setAuthenticating(false);
+      })
+      .catch(() => {
+        setAuthenticating(false);
+      });
+  }, []);
+
+  return (
+    !authenticating && (
+      <Route
+        {...rest}
+        render={
+          ({ location }) =>
+            auth.user && location.pathname.trim("/") === auth.user.user_role ? (
+              children
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/",
+                  state: { from: location },
+                }}
+              />
+            )
+          // eslint-disable-next-line react/jsx-curly-newline
+        }
+      />
+    )
+  );
+};
