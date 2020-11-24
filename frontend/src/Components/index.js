4ab9905a2326fb4e08a593cb1f491413665e320e
ForkBombGIT <erosdipede@gmail.com>
2020-11-23 19:41:18 -0500

    CODE Add frontend components to support professor creation

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,3 +1,5 @@
 export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
 export { default as NotFound } from "./NotFound";
+export { default as UserList } from "./UserList";
+export { default as ModelCreationForm } from "./ModelCreationForm";
