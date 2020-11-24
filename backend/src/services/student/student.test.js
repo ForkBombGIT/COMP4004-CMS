a60f81c4d29eea35a97fb5e2e3350b39e6a0bf68
ForkBombGIT <erosdipede@gmail.com>
2020-11-24 10:53:00 -0500

    TEST Add service tests to support student removal

--- backend/src/services/student/student.test.js
+++ backend/src/services/student/student.test.js
@@ -1,8 +1,40 @@
 const app = require('app');
+const { checkObjectContains } = require('utils/helpers.js');
 
 describe('"student" service', () => {
+  let service;
+
+  beforeAll(() => {
+    service = app.service('student');
+  });
+
   it('registered the service', () => {
-    const service = app.service('student');
     expect(service).toBeTruthy();
+  });
+  it('removes a student', async () => {
+    // Remove record if it exists
+
+    const testStudent = {
+      id: '99d21763-f1e1-4bba-b163-a4a56dba4257',
+      name: 'james',
+    };
+
+    try { 
+      await service.remove(testStudent.id);
+    } catch (e) {console.log('No record to remove');}
+
+    let createdRecord = await service.create(testStudent);
+
+    expect(checkObjectContains(createdRecord, testStudent)).toBeTruthy();
+
+    try { 
+      await service.remove(testStudent.id);
+    } catch (e) {console.log('No record to remove');}
+
+    let professor = await service.find({
+      query: {id: testStudent.id}}
+    );
+    expect(professor.length).toBe(0);
+
   });
 });
