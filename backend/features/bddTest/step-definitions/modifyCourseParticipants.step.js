4b6a50668d188beb8fbdc2c16577c7c3e2b24a4e
Liannus <josh-gorman@hotmail.com>
2020-11-25 14:32:52 -0500

    ACCEPTANCE-TEST Add Administrator Action: Add Professor to Course

---
+++ backend/features/bddTest/step-definitions/modifyCourseParticipants.step.js
@@ -0,0 +1,93 @@
+
+module.exports = function () {
+    this.Then(/^I click on unregister professor button$/, function (callback) {
+    driver.findElement(By.id('unregister-professor-button')).click();
+    driver.sleep(1000).then(() => {
+      callback();
+    });
+    });
+
+    this.Given(/^"([^"]*)" is a professor of course "([^"]*)"$/, function (arg1, arg3, callback) {
+        const app = shared.app.api.service('course');
+        const app2 = shared.app.api.service('professor')
+        app.find({
+            query: {
+                name: arg3
+            }
+        }
+        ).then((course) => {
+            app2.find({
+                query: {
+                    name: arg1
+                }
+            }).then((prof) => {
+                app.patch(course[0].id, { professorId: prof[0].id });
+                callback()
+            })
+        })
+    });
+
+    this.Given(/^"([^"]*)" is NOT a professor of course "([^"]*)"$/, function (arg1, arg3, callback) {
+        const app = shared.app.api.service('course');
+        app.find({
+            query: {
+                name:arg3
+            }
+        }
+        ).then((record) => {
+            console.log("stuff: ")
+            console.log(record)
+            app.patch(record[0].id, { professorId: null }).then(() => {
+                callback()
+            });
+        })
+    });
+
+
+    this.Given(/^"([^"]*)" is a student of course "([^"]*)"$/, function (arg1, arg3, callback) {
+        const app = shared.app.api.service('course');
+        const app2 = shared.app.api.service('student');
+        const app3 = shared.app.api.service('enrolled');
+        app.find({
+            query: {
+                name:arg3
+            }
+        }
+        ).then((course) => {
+            app2.find({
+                query: {
+                    name:arg1
+                }
+            }).then((student) => {
+            console.log(student);
+                app3.create({ studentId: student[0].id, courseId: course[0].id }).then((e) => {
+                    console.log(e)
+                    callback()
+                });
+            })
+        })
+    });
+    
+
+    this.Given(/^"([^"]*)" is NOT a student of course "([^"]*)"$/, function (arg1, arg3, callback) {
+        const app = shared.app.api.service('course');
+        const app2 = shared.app.api.service('student');
+        const app3 = shared.app.api.service('enrolled');
+        app.find({
+            query: {
+                name: arg3
+            }
+        }
+        ).then((course) => {
+            app2.find({
+                query: {
+                    name: arg1
+                }
+            }).then((student) => {
+                app3.remove(null, { query: { studentId: student[0].id, courseId: course[0].id } }).then(() => {
+                callback()
+                });
+            })
+        })
+    });
+};
