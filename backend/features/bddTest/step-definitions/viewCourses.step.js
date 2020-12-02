eab0943ebb85ac36c0288bc0511f3fe52b08b46b
ForkBombGIT <erosdipede@gmail.com>
2020-12-02 17:39:43 -0500

    ACCEPTANCE-TEST Add tests to support deliverable creation
    Fix viewCourses course amount expectation

--- backend/features/bddTest/step-definitions/viewCourses.step.js
+++ backend/features/bddTest/step-definitions/viewCourses.step.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     this.Then(/^I should see courses I am assigned to$/, function (callback) {
         driver.findElements(by.className("MuiListItem-container")).then((elements)=>{
-            expect(elements.length).to.equal(2);
+            expect(elements.length).to.equal(1);
             callback();
         })
       });
